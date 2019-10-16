4518cd1966991a68012ac7ff3ea24762ba841d79
huangming <wangbandi@gmail.com>
2019-10-16 22:09:34 +0800

    gnome-terminal transparency tool done

--- tools/tools/transparent-terminal.sh
+++ tools/tools/transparent-terminal.sh
@@ -1,4 +1,15 @@
 UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \') 
 CURRENT_TRANSPARENCY=$(gsettings get org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ background-transparency-percent)
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ use-transparent-background false
-
+# gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ use-transparent-background false
+NEW_TRANSPARENCY=$CURRENT_TRANSPARENCY
+if [ $1 = "+" ]
+then
+    NEW_TRANSPARENCY=`expr $CURRENT_TRANSPARENCY + $2`
+elif [ $1 = "-" ]
+then
+    NEW_TRANSPARENCY=`expr $CURRENT_TRANSPARENCY - $2`
+else
+    NEW_TRANSPARENCY=$2
+fi
+echo $NEW_TRANSPARENCY
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ background-transparency-percent $NEW_TRANSPARENCY
